9c31d002d38982e169e9e45470ef9e5a6413dc21
unfettered-one <pakhaleyash7@gmail.com>
2025-08-20 18:51:43 +0000

    fix: top level packhe of lib

--- errorhub/setup.py
+++ errorhub/setup.py
@@ -6,8 +6,9 @@ setup(
     author="Yash Pakhale",
     author_email="pakhaleyash7@gmail.com",
     url="https://github.com/unfettered-one/Error-Hub.git",
-    version="0.1.0",
-    packages=find_packages(),
+    version="0.1.1",
+    packages=find_packages(where="errorhub"),
+    package_dir={"": "errorhub"},
     include_package_data=True,
     install_requires=["fastapi==0.116.1", "pydantic==2.11.7", "dotenv==0.9.9", "requests==2.32.5"],
     entry_points={
