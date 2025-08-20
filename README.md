336c021c7850358f32aab7214d064dd2a0f48c63
unfettered-one <pakhaleyash7@gmail.com>
2025-08-20 18:00:13 +0000

    feat: make it ready to publish

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Error-Hub
-Custom library publicly deployed through pip. One shot library to handle errors like a champ, with a decorator to wrap up api functions, avoiding try catch again and again
+Custom library publicly deployed through pip. One shot library to handle errors like a champ, with a decorator to wrap up api functions, avoid implemeting try catch again and again for every other api
--- errorhub/setup.py
+++ errorhub/setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="errorhub",
-    version="0.0.1",
+    description="Error and Exception Handling lib",
+    author="Yash Pakhale",
+    author_email="pakhaleyash7@gmail.com",
+    url="https://github.com/unfettered-one/Error-Hub.git",
+    version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=["fastapi==0.116.1", "pydantic==2.11.7", "dotenv==0.9.9", "requests==2.32.5"],
     entry_points={
         "console_scripts": [],
