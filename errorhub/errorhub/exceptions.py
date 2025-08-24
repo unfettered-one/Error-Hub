e5bd039a6968c275fa9273390b6c2a5a47245bc3
unfettered-one <pakhaleyash7@gmail.com>
2025-08-24 11:09:28 +0000

    chore: lint score 10

--- errorhub/errorhub/exceptions.py
+++ errorhub/errorhub/exceptions.py
@@ -2,16 +2,21 @@
 All Custom exception you will need in your life
 """
 
-from datetime import datetime, timezone
+from datetime import datetime, UTC
 import uuid
-from errorhub.models import ErrorDetail, ErrorSeverity, EnvironmentEnum
 import logging
+
+from errorhub.models import ErrorDetail, ErrorSeverity, EnvironmentEnum
 
 
 LOGGER = logging.getLogger(__name__)
 
 
 class ErrorHubException(Exception):
+    """
+    Super Exception for all ErrorHub related errors.
+    """
+
     def __init__(
         self,
         service: str,
@@ -21,7 +26,7 @@ class ErrorHubException(Exception):
         severity: ErrorSeverity = ErrorSeverity.MEDIUM,
         environment: EnvironmentEnum = EnvironmentEnum.DEVELOPMENT,
         trace_id: str | None = None,
-        context: dict = {},
+        context: dict | None = None,
     ):
         """
         Super Exception Inherited by every custom Excetion in error hub
@@ -34,7 +39,7 @@ class ErrorHubException(Exception):
             severity=severity,
             environment=environment,
             trace_id=trace_id or str(uuid.uuid4()),
-            timestamp=datetime.now(timezone.utc),
+            timestamp=datetime.now(UTC).isoformat(),
             context=context,
         )
         super().__init__(message)
@@ -53,49 +58,91 @@ class ErrorHubException(Exception):
         return self.error_detail.model_dump()
 
     def __str__(self):
-        return f"[{self.error_detail.service}] {self.error_detail.error_type}: {self.error_detail.code} | {self.error_detail.message}"
+        """
+        String format of error in single line
+        """
+        return (
+            f"[{self.error_detail.service}] {self.error_detail.error_type}:"
+            f" {self.error_detail.code} | {self.error_detail.message}"
+        )
 
 
 class NotFoundException(ErrorHubException):
+    """
+    Exception raised when a requested resource is not found. 404
+    """
+
     def __init__(self, service: str, message: str = "Resource not found", **kwargs):
         super().__init__(service=service, error_type="NotFound", message=message, code=404, **kwargs)
 
 
 class UnauthorizedException(ErrorHubException):
+    """
+    Exception raised when a request is unauthorized. 401
+    """
+
     def __init__(self, service: str, message: str = "Unauthorized", **kwargs):
         super().__init__(service=service, error_type="Unauthorized", message=message, code=401, **kwargs)
 
 
 class BadRequestException(ErrorHubException):
+    """
+    Exception raised when a request is invalid. 400
+    """
+
     def __init__(self, service: str, message: str = "Bad request", **kwargs):
         super().__init__(service=service, error_type="BadRequest", message=message, code=400, **kwargs)
 
 
 class InternalServerErrorException(ErrorHubException):
+    """
+    Exception raised when an internal server error occurs. 500
+    """
+
     def __init__(self, service: str, message: str = "Internal server error", **kwargs):
         super().__init__(service=service, error_type="InternalServerError", message=message, code=500, **kwargs)
 
 
 class ForbiddenException(ErrorHubException):
+    """
+    Exception raised when a request is forbidden. 403
+    """
+
     def __init__(self, service: str, message: str = "Forbidden", **kwargs):
         super().__init__(service=service, error_type="Forbidden", message=message, code=403, **kwargs)
 
 
 class ConflictException(ErrorHubException):
+    """
+    Exception raised when a request conflicts with the current state. 409
+    """
+
     def __init__(self, service: str, message: str = "Conflict", **kwargs):
         super().__init__(service=service, error_type="Conflict", message=message, code=409, **kwargs)
 
 
 class UnprocessableEntityException(ErrorHubException):
+    """
+    Exception raised when a request is unprocessable. 422
+    """
+
     def __init__(self, service: str, message: str = "Unprocessable entity", **kwargs):
         super().__init__(service=service, error_type="UnprocessableEntity", message=message, code=422, **kwargs)
 
 
 class ServiceUnavailableException(ErrorHubException):
+    """
+    Exception raised when a service is unavailable. 503
+    """
+
     def __init__(self, service: str, message: str = "Service unavailable", **kwargs):
         super().__init__(service=service, error_type="ServiceUnavailable", message=message, code=503, **kwargs)
 
 
 class GatewayTimeoutException(ErrorHubException):
+    """
+    Exception raised when a gateway timeout occurs. 504
+    """
+
     def __init__(self, service: str, message: str = "Gateway timeout", **kwargs):
         super().__init__(service=service, error_type="GatewayTimeout", message=message, code=504, **kwargs)
--- errorhub/errorhub/models.py
+++ errorhub/errorhub/models.py
@@ -4,28 +4,39 @@ Basic unit of errorhub life...everything depends on this model
 
 from enum import Enum
 from pydantic import BaseModel
-from datetime import datetime
 
 
 class ErrorSeverity(str, Enum):
+    """
+    Represents the severity levels of errors.
+    """
+
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"
 
 
 class EnvironmentEnum(str, Enum):
+    """
+    Represents the different environments in which the application can run.
+    """
+
     DEVELOPMENT = "development"
     STAGING = "staging"
     PRODUCTION = "production"
 
 
 class ErrorDetail(BaseModel):
+    """
+    Represents the details of an error. Life of errohub library depends on this model.
+    """
+
     service: str
     error_type: str
     code: int
     message: str
-    timestamp: datetime
+    timestamp: str
     trace_id: str | None = None
     severity: ErrorSeverity
     environment: EnvironmentEnum
-    context: dict = {}
+    context: dict | None = None
